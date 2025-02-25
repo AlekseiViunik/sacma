c7bc3cdfc6f704c36e93da4906304e2038088ee4
Aleksei VIunik <vjunik88@gmail.com>
2025-02-25 13:53:24 +0100

    add main window settings

---
+++ settings/settings.py
@@ -0,0 +1,16 @@
+MAIN_WIN_WIDTH = 600
+MAIN_WIN_HEIGHT = 300
+MAIN_WIN_TITLE = "SACMA"
+
+MAIN_WIN_BUTTONS = [
+    "Fiancate",
+    "Travi",
+    "Tasselli",
+    "Satellitare",
+    "Pianetti",
+    "Gragliato",
+    "Travi di battuta",
+    "Angolari per automatici",
+    "Gravita leggera",
+    "Option di sicurezza"
+]
