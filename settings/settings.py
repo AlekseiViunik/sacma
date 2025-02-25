72397abdc4e6433e07ff331dd5cc7ef806645ee5
Aleksei VIunik <vjunik88@gmail.com>
2025-02-25 14:11:23 +0100

    add new settings

--- settings/settings.py
+++ settings/settings.py
@@ -14,3 +14,17 @@ MAIN_WIN_BUTTONS = [
     "Gravita leggera",
     "Option di sicurezza"
 ]
+
+# Main frame
+MAIN_WIN_FRAME_COL_NUM = 3
+
+# Buttons
+BUTTON_HEIGHT = 2
+BUTTON_COLOR = "#d9d9d9"
+BUTTON_PADX = 10
+BUTTON_PADY = 10
+BUTTON_STICKY = "nsew"
+BUTTON_RELIEF = "raised"
+
+# Grid
+GRID_WEIGHT = 1
