1407e607c2c6cea0026b2e505ae492083bf60cd3
Aleksei VIunik <vjunik88@gmail.com>
2025-02-25 16:38:22 +0100

    Add window creator

---
+++ gui/window_creator.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+from settings import settings as set
+
+
+class WindowCreator:
+    def __init__(self, window, select_fields, input_fields):
+        self.window = window
+        self.select_fields = select_fields
+        self.input_fields = input_fields
+        self.entries = {}
+
+    def create_ui(self):
+        frame = tk.Frame(
+            self.window,
+            bg=set.FRAME_BG_COLOR,
+            padx=set.FRAME_PADX,
+            pady=set.FRAME_PADY
+        )
+        frame.pack(expand=True, fill=tk.BOTH)
+
+        # Поля с выпадающими списками
+        for i, (label, values) in enumerate(self.select_fields.items()):
+            if not values:
+                continue
+            tk.Label(frame, text=label, bg=set.LABEL_BG_COLOR).grid(
+                row=i,
+                column=set.LABEL_NAME_COLUMN,
+                sticky=set.LABEL_STICKY,
+                pady=set.LABEL_PADY
+            )
+            var = tk.StringVar(value=values[0])
+            dropdown = tk.OptionMenu(frame, var, *values)
+            dropdown.grid(
+                row=i,
+                column=set.DROPDOWN_COLUMN,
+                sticky=set.DROPDOWN_STICKY,
+                padx=set.DROPDOWN_PADX
+            )
+            self.entries[label] = var
+
+        start_row = len(self.select_fields)  # Начинаем после селектов
+
+        # Поля для ввода
+        for i, label in enumerate(self.input_fields):
+            tk.Label(frame, text=label, bg=set.LABEL_BG_COLOR).grid(
+                row=start_row + i,
+                column=set.LABEL_NAME_COLUMN,
+                sticky=set.LABEL_STICKY,
+                pady=set.LABEL_PADY
+            )
+            entry = tk.Entry(frame)
+            entry.grid(
+                row=start_row + i,
+                column=set.ENTRY_COLUMN,
+                sticky=set.ENTRY_STICKY,
+                padx=set.ENTRY_PADX
+            )
+            self.entries[label] = entry
+
+            # Добавляем "mm" для размеров
+            if label in set.dimensions_need_mm:
+                tk.Label(
+                    frame,
+                    text=set.LABEL_MM_TEXT,
+                    bg=set.LABEL_BG_COLOR
+                ).grid(
+                    row=start_row + i,
+                    column=set.LABEL_MM_COLUMN,
+                    sticky=set.LABEL_STICKY
+                )
+
+        # Настройка растяжения колонок
+        for i in range(set.COL_NUM):
+            frame.columnconfigure(i, weight=set.GRID_WEIGHT)
+
+        return self.entries
