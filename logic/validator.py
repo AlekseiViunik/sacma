cabffb0620f0f04cb501d54fb00361132d3fe322
Aleksei VIunik <vjunik88@gmail.com>
2025-02-26 11:40:25 +0100

    creeate validator

---
+++ logic/validator.py
@@ -0,0 +1,20 @@
+class Validator:
+
+    @staticmethod
+    def validate(rul_key, rul_value, value):
+        match rul_key:
+            case "min":
+                try:
+                    value = int(value)
+                except ValueError:
+                    return False
+                if value < rul_value:
+                    return False
+            case "max":
+                try:
+                    value = int(value)
+                except ValueError:
+                    return False
+                if value > rul_value:
+                    return False
+        return True
