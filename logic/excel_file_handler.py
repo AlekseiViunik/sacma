5ba0e8193897cbab93a36889daabfc3351b0f175
Aleksei VIunik <vjunik88@gmail.com>
2025-02-26 12:06:32 +0100

    add TODOs

--- logic/excel_file_handler.py
+++ logic/excel_file_handler.py
@@ -14,6 +14,9 @@ class ExcelFileHandler:
         self.worksheet = None
 
     def prepare_data_for_excel(self):
+        # TODO Добавить докстринги
+        # TODO Добавить обработку для других типов полок
+
         if self.part_of_the_shelf.lower() == "travi":
             if not self.check_data(
                 set.TRAVI_RULES[self.data[set.TRAVI_TYPE_KEY]]
@@ -27,6 +30,7 @@ class ExcelFileHandler:
             return data_prepared
 
     def prepare_dict(self, cells):
+        # TODO Добавить докстринги
         data_prepared = {}
         for name, cell in cells.items():
             if name in self.data:
@@ -34,40 +38,55 @@ class ExcelFileHandler:
                 data_prepared[cell] = self.data[name]
         return data_prepared
 
+    def get_result_cells(self):
+        # TODO Переделать на словарь
+        # TODO Добавить обработку для других типов полок
+        # TODO Добавить докстринги
+        if self.part_of_the_shelf.lower() == "travi":
+            match self.data[set.TRAVI_TYPE_KEY]:
+                case set.TRAVI_TYPE_TG:
+                    price_cell = set.TRAVI_CELLS_TG["Prezzo"]
+                    weight_cell = set.TRAVI_CELLS_TG["Peso"]
+        return price_cell, weight_cell
+
     def process_excel(self):
         """Открывает Excel, записывает данные, сохраняет, затем открывает
         снова, считывает результаты и закрывает файл."""
 
+        # TODO Файл должен в будущем браться из облака и обновляться 1 раз/день
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         FILE_PATH = os.path.join(BASE_DIR, "..", "files", "listini.xlsx")
         FILE_PATH = os.path.abspath(FILE_PATH)
 
+        # Подготавливаем данные для записи в Excel
         data_prepared = self.prepare_data_for_excel()
         if not data_prepared:
             return None, None
 
+        # Открываем Excel
         excel = win32.Dispatch("Excel.Application")
         excel.Visible = False  # Запуск в фоновом режиме
-
+        # Без обновления связей
         wb = excel.Workbooks.Open(FILE_PATH, UpdateLinks=0)
 
         try:
             wb.Unprotect()  # Снимаем защиту с книги
-            wb.Sheets("Listino Travi").Unprotect()  # Снимаем защиту с листа
+            wb.Sheets(self.worksheet).Unprotect()  # Снимаем защиту с листа
         except Exception:
             pass
 
-        sheet = wb.Sheets("Listino Travi")
+        sheet = wb.Sheets(self.worksheet)
 
         for cell, value in data_prepared.items():
             sheet.Range(cell).Value = value
 
         wb.RefreshAll()  # Обновляем связи
         excel.CalculateUntilAsyncQueriesDone()
+        price_cell, weight_cell = self.get_result_cells()
+        price = sheet.Range(price_cell).Value
+        weight = sheet.Range(weight_cell).Value
 
-        price = sheet.Range("E4").Value
-        weight = sheet.Range("E6").Value
-
+        # TODO Вынести в отдельный метод
         if price > 0:
             price = Decimal(price).quantize(
                 Decimal("0.01"),
@@ -87,6 +106,7 @@ class ExcelFileHandler:
         return price, weight
 
     def check_data(self, rules):
+        # TODO Добавить докстринги
         for key, value in self.data.items():
             key = key.lower()
             if key in rules:
