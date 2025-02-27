4278b54c593cde4e5f23fe120c23f45aa89a089b
Aleksei VIunik <vjunik88@gmail.com>
2025-02-27 09:26:49 +0100

    add logging divider

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from gui.main_window import App
+from logic.logger import logger
 
 if __name__ == "__main__":
     import tkinter as tk
+    logger.info("============================================================")
 
     root = tk.Tk()
     app = App(root)
