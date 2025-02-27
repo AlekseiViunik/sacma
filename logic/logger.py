08293f5b7bf54bbab84e3db90df60ba98ac750bb
Aleksei VIunik <vjunik88@gmail.com>
2025-02-27 11:13:26 +0100

    add check_log_size method and remove stram handler

--- logic/logger.py
+++ logic/logger.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from settings import settings as set
+
 # Путь к файлу логов
 LOG_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -10,13 +12,23 @@ LOG_DIR = os.path.join(
 os.makedirs(LOG_DIR, exist_ok=True)  # Создаём папку logs, если её нет
 LOG_FILE = os.path.join(LOG_DIR, "app.log")
 
+
+def check_log_size():
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        if len(lines) >= set.MAX_LOG_LINES:
+            with open(LOG_FILE, "w", encoding="utf-8") as f:
+                f.write("")  # Очищаем файл
+
+
 # Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",  # Формат сообщения
     handlers=[
         logging.FileHandler(LOG_FILE, encoding="utf-8"),  # Логи в файл
-        logging.StreamHandler()  # Логи в консоль
     ]
 )
 
