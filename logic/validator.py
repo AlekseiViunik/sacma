f3291cfbda835f1e22f544073b8faa713844b52b
Aleksei VIunik <vjunik88@gmail.com>
2025-02-27 10:14:22 +0100

    add loggin to the validator

--- logic/validator.py
+++ logic/validator.py
@@ -1,23 +1,41 @@
+from logic.logger import logger as log
+
+
 class Validator:
 
     @staticmethod
-    def validate(rul_key, rul_value, value):
-        match rul_key:
+    def validate(rule_key, rule_value, value):
+        log.info(f"Check {rule_key} = {value}")
+        match rule_key:
             case "min":
+                log.info(f"'{rule_key}' should be more than {rule_value}")
                 try:
                     value = int(value)
                 except ValueError:
+                    log.error(f"{rule_key}={value} is not numeric")
                     return False
-                if value < rul_value:
+                if value < rule_value:
+                    log.error(
+                        f"{rule_key}={value} is less than min possible"
+                        f"({rule_value})"
+                    )
                     return False
             case "max":
+                log.info(f"'{rule_key}' should be less than {rule_value}")
                 try:
                     value = int(value)
                 except ValueError:
+                    log.error(f"{rule_key}={value} is not numeric")
                     return False
-                if value > rul_value:
+                if value > rule_value:
+                    log.error(
+                        f"{rule_key}={value} is greater than max possible"
+                        f"({rule_value})"
+                    )
                     return False
             case "numeric":
+                log.info(f"'{rule_key}' should be numeric")
                 if not str(value).isnumeric():
+                    log.error(f"{rule_key}={value} is not numeric")
                     return False
         return True
