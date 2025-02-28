0c2de0103b48ab2c7234881fff5f6b9b4fb7c2ba
Aleksei VIunik <vjunik88@gmail.com>
2025-02-28 16:22:19 +0100

    add get_class_name_if_exists method to helper

--- gui/helper.py
+++ gui/helper.py
@@ -1,8 +1,12 @@
+import importlib
+from typing import Tuple
+
+
 class Helper:
     """Вспомогательный класс с утилитарными методами."""
 
     @staticmethod
-    def get_type_class_name(name: str) -> str:
+    def get_type_class_name(name: str) -> Tuple[str]:
         """Преобразует строку в имя класса в CamelCase.
 
         Примеры:
@@ -20,4 +24,18 @@ class Helper:
             str
                 Строка в CamelCase (как имя класса).
         """
-        return "".join(word.capitalize() for word in name.split())
+        class_file = "_".join(word.lower() for word in name.split())
+        class_name = "".join(word.capitalize() for word in name.split())
+        return class_name, class_file
+
+    @staticmethod
+    def get_class_name_if_exists(name):
+        class_name, class_file = Helper.get_type_class_name(name)
+        module_path = f"gui.type_classes.{class_file}"
+
+        try:
+            module = importlib.import_module(module_path)
+            class_ = getattr(module, class_name)
+            return class_
+        except (ModuleNotFoundError, AttributeError):
+            return False
