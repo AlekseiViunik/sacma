2311c6ba6dfaa6487d0ae1ff644c85aa4ee90e61
Aleksei VIunik <vjunik88@gmail.com>
2025-03-03 11:40:45 +0100

    modify Travi class

--- gui/type_classes/travi.py
+++ gui/type_classes/travi.py
@@ -5,55 +5,54 @@ from gui.window_creator import WindowCreator
 from logic.excel_file_handler import ExcelFileHandler
 from logic.logger import logger as log
 from settings import settings as set
-from .abstract_type_class import AbstractTypeClass
+from abstract_base_type import AbstractBaseType
 
 
-class Travi(AbstractTypeClass):
-    def __init__(self, root, name):
-        self.root = root
-        self.name = name
-        self.window_width = set.TRAVI_WIN_WIDTH
-        self.window_height = set.TRAVI_WIN_HEIGHT
-        self.type_choice = set.TRAVI_CHOICE
+class Travi(AbstractBaseType):
+    def __init__(self, root, type):
+        super().__init__(root, type)
 
-    def open_window(self):
-        log.info(f"Button {self.name} is pressed.")
-        self.root.withdraw()
-        self.window = tk.Toplevel(self.root)
-        self.window.title(self.name)
-        self.window.geometry(f"{self.window_width}x{self.window_height}")
-        self.center_window(self.window_width, self.window_height)
-        self.create_components()
-
+    def create_components(self):
+        select_options = (
+            self.type_choice[
+                "choices"
+            ][set.TRAVI_TYPE_TG]["available_params"]["select"]
+        )
+        input_options = (
+            self.type_choice[
+                "choices"
+            ][set.TRAVI_TYPE_TG]["available_params"]["input"]
+        )
+        always_on = (
+            self.type_choice["always_on"]
+        )
+        creator = WindowCreator(
+            self.window,
+            select_options,
+            input_options,
+            always_on,
+        )
+        creator.create_ui()
+        self.entries = creator.entries
         btn_invia = tk.Button(
             self.window,
             text=set.BUTTON_INVIA_TITLE,
             width=set.BUTTON_WIDTH,
             bg=set.BUTTON_COLOR,
             relief=set.BUTTON_RELIEF,
-            command=self.calculate
+            command=self.calculate()
         )
         btn_invia.pack(
-            side=set.BUTTON_INVIA_SIDE,
-            anchor=set.BUTTON_INVIA_ANCHOR,
-            padx=set.BUTTON_PADX,
-            pady=set.BUTTON_PADY
-        )
+                side=set.BUTTON_INVIA_SIDE,
+                anchor=set.BUTTON_INVIA_ANCHOR,
+                padx=set.BUTTON_PADX,
+                pady=set.BUTTON_PADY
+            )
 
         self.window.protocol(
             set.ON_CLOSING_WINDOW,
             lambda: Helper(self.root).on_close(self.window)
         )
-
-    def create_components(self):
-        creator = WindowCreator(
-            self.window,
-            self.type_choice[set.TRAVI_TYPE_TG]['select'],
-            self.type_choice[set.TRAVI_TYPE_TG]['input'],
-            self.type_choice['always_on'],
-        )
-        creator.create_ui()
-        self.entries = creator.entries
 
     def calculate(self):
         entries_dict = {
