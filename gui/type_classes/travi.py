b6bef28a338bd666c4ac6182dd128071523b8684
Aleksei VIunik <vjunik88@gmail.com>
2025-03-04 14:26:18 +0100

    remove open_response_window wrom travi class

--- gui/type_classes/travi.py
+++ gui/type_classes/travi.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-
 from gui.helper import Helper
 from gui.widget_creator import WidgetCreator
 from logic.excel_file_handler import ExcelFileHandler
@@ -44,37 +42,3 @@ class Travi(AbstractBaseType):
         excel = ExcelFileHandler(self.name, entries_dict)
         cost, weight = excel.process_excel()
         self.open_response_window(cost, weight)
-
-    def open_response_window(self, cost, weight):
-        result_window = tk.Toplevel(self.root)
-        result_window.title("Risultato")  # Заголовок окна
-        self.center_window(300, 150, result_window)  # Центрируем окно
-
-        # Формируем текст для отображения
-        prezzo_text = (
-            f"{set.PRICE}: {cost} €" if cost else set.PRICE_NOT_FOUND
-        )
-        peso_text = (
-            f"{set.WEIGHT}: {weight} Kg" if weight else set.WEIGHT_NOT_FOUND
-        )
-
-        # Выводим текст
-        tk.Label(
-            result_window,
-            text=prezzo_text,
-            font=("Arial", 12)
-        ).pack(pady=5)
-        tk.Label(
-            result_window,
-            text=peso_text,
-            font=("Arial", 12)
-        ).pack(pady=5)
-
-        # Кнопка "OK", которая закроет окно
-        tk.Button(
-            result_window,
-            width=set.BUTTON_WIDTH,
-            bg=set.BUTTON_COLOR,
-            text="OK",
-            command=result_window.destroy
-        ).pack(pady=10)
