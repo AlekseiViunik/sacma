b6a51facb79fdafabd0c9b60634f96bb428a9649
Aleksei VIunik <vjunik88@gmail.com>
2025-03-04 15:18:13 +0100

    remove create_components from еру inheritor classes

--- gui/type_classes/travi.py
+++ gui/type_classes/travi.py
@@ -1,38 +1,12 @@
-from gui.helper import Helper
-from gui.widget_creator import WidgetCreator
+
 from logic.excel_file_handler import ExcelFileHandler
 from logic.logger import logger as log
-from settings import settings as set
 from abstract_base_type import AbstractBaseType
 
 
 class Travi(AbstractBaseType):
     def __init__(self, root, type):
         super().__init__(root, type)
-
-    def create_components(self):
-        select_options, input_options, always_on = (
-            (
-                self.get_default_options(set.TRAVI_TYPE_TG, option) for
-                option in ["select", "input", "always_on"]
-            )
-        )
-
-        creator = WidgetCreator(
-            self.window,
-            select_options,
-            input_options,
-            always_on,
-        )
-        creator.create_ui()
-        self.entries = creator.entries
-
-        creator.create_invia_button(self.calculate)
-
-        self.window.protocol(
-            set.ON_CLOSING_WINDOW,
-            lambda: Helper(self.root).on_close(self.window)
-        )
 
     def calculate(self):
         entries_dict = {
