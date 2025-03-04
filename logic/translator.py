3b9c1fc1daafc975ac881cf22d83000cc6236678
Aleksei VIunik <vjunik88@gmail.com>
2025-03-04 18:27:26 +0100

    beautifulize translator

--- logic/translator.py
+++ logic/translator.py
@@ -7,7 +7,6 @@ class Translator:
     def translate_dict(value, only_keys=True):
         if only_keys:
             return {
-                set.DICTIONARY.get(key, key):
-                value for key, value in value.items()
+                set.DICTIONARY.get(key, key): val for key, val in value.items()
             }
         return None
