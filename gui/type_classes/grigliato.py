980b4b949b34d78e3216059c47c76cacaad931bc
Aleksei VIunik <vjunik88@gmail.com>
2025-03-05 16:28:38 +0100

    add logic to the grigliato class

--- gui/type_classes/grigliato.py
+++ gui/type_classes/grigliato.py
@@ -1,3 +1,99 @@
-class Grigliato:
-    def open_window():
-        pass
+from decimal import Decimal
+from logic.excel_file_handler import ExcelFileHandler
+from logic.logger import logger as log
+from abstract_base_type import AbstractBaseType
+
+
+class Grigliato(AbstractBaseType):
+    def __init__(self, root, type):
+        super().__init__(root, type)
+
+    def calculate(self) -> None:
+        """
+        Метод обработки данных, указанных полльзователем.
+        1. Преобразует entries в словарь.
+        2. Создает объект ExcelFileHandler.
+        3. Вызывает метод process_excel у созданного объекта.
+        4. Открывает окно с результатами.
+        """
+        entries_dict = {
+            key: entry.get() for key, entry in self.entries.items()
+        }
+        data = self.type_choice["choices"][entries_dict["Tipo"]]
+        rules = data["rules"]
+        worksheet = data["worksheet"]
+        cells_output = self.evaluate_output_cells(
+            data["cells_output"],
+            entries_dict
+        )
+        log.info(f"Entries: {entries_dict}")
+        excel = ExcelFileHandler(
+            entries_dict,
+            rules,
+            worksheet,
+            cells_output=cells_output,
+        )
+        cost_m, weight_m = excel.process_excel()
+        cost, weight = self.calculate_total_cost(
+            cost_m,
+            weight_m,
+            entries_dict["Lunghezza"]
+        ) if cost_m and weight_m else (None, None)
+        self.open_response_window(cost, weight)
+
+    def evaluate_output_cells(
+        self,
+        cells_output: dict,
+        entries_dict: dict
+    ) -> dict:
+        """Метод для вычисления ячеек вывода.
+
+        Parameters
+        ----------
+            cells_output : dict
+                словарь с ячейками вывода.
+            entries_dict : dict
+                словарь с данными, введенными пользователем.
+
+        Return
+        ------
+            cells_output : dict
+                словарь с вычисленными ячейками вывода.
+        """
+        cells = {
+            "price": cells_output[
+                entries_dict["Base"]
+            ][entries_dict["Spessore"]]["price"],
+            "weight": cells_output[
+                entries_dict["Base"]
+            ][entries_dict["Spessore"]]["weight"],
+        }
+        return cells
+
+    def calculate_total_cost(
+        self,
+        cost_m: Decimal,
+        weight_m: Decimal,
+        length
+    ) -> tuple:
+        """Метод для вычисления общей стоимости и веса.
+
+        Parameters
+        ----------
+            cost_m : Decimal
+                стоимость за метр.
+            weight_m : Decimal
+                вес за метр.
+            length : Decimal
+                длина.
+
+        Return
+        ------
+            cost : Decimal
+                общая стоимость.
+            weight : Decimal
+                общий вес.
+        """
+        cost = cost_m * int(length) / 1000
+        weight = weight_m * int(length) / 1000
+        return cost, weight
