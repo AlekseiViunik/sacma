f969b20ab2bfc4e036a6844026ef5fc129d9d8b5
Aleksei VIunik <vjunik88@gmail.com>
2025-03-05 12:17:50 +0100

    add docstring to the json handler

--- logic/json_file_handler.py
+++ logic/json_file_handler.py
@@ -1,12 +1,41 @@
 import json
+from typing import Any
+
+from logic.logger import logger as log
 
 
 class JsonFileHandler:
-    def __init__(self, file):
+    """
+    Класс JsonFileHandler используется для работы с JSON-файлами.
+
+    Attributes
+    ----------
+    file : str
+        Путь к JSON-файлу или имя файла, если он лежит в корне проекта.
+
+    Methods
+    -------
+    read_value_by_key(key: str) -> Any
+        Читает JSON и возвращает значение по переданному ключу.
+    """
+
+    def __init__(self, file: str):
         self.file = file
 
-    def read_value_by_key(self, key: str):
-        """Читает JSON и возвращает значение по переданному ключу."""
+    def read_value_by_key(self, key: str) -> Any:
+        """
+        Читает JSON и возвращает значение по переданному ключу.
+
+        Parameters
+        ----------
+        key : str
+            Ключ, по которому нужно вернуть значение из файла.
+
+        Returns
+        -------
+        Any
+            Возвращает значение по ключу или None, если ключ не найден.
+        """
         try:
             with open(self.file, "r", encoding="utf-8") as f:
                 data = json.load(f)  # Загружаем JSON в словарь
@@ -14,8 +43,8 @@ class JsonFileHandler:
             return data.get(key, None)  # Возвращаем значение по ключу или None
 
         except FileNotFoundError:
-            print(f"❌ Файл '{self.file}' не найден.")
+            log.error(f"❌ Файл '{self.file}' не найден.")
             return None
         except json.JSONDecodeError:
-            print(f"❌ Ошибка чтения JSON в файле '{self.file}'.")
+            log.error(f"❌ Ошибка чтения JSON в файле '{self.file}'.")
             return None
