92c78ae23151239805b491984ec6c763be1088f4
Aleksei VIunik <vjunik88@gmail.com>
2025-03-06 12:01:28 +0100

    call login window before launching the main window

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+from gui.login_window import LoginWindow
 from gui.main_window import App
 from logic.logger import logger, check_log_size
+
 
 if __name__ == "__main__":
     import tkinter as tk
     check_log_size()
+
+    root = tk.Tk()
+
+    # Открываем окно авторизации
+    login_window = LoginWindow(root)
+    root.mainloop()
+
     logger.info("============================================================")
 
     root = tk.Tk()
