d932b2a5dffa61ed346bf2ca9b93ef295371188b
Aleksei VIunik <vjunik88@gmail.com>
2025-03-07 12:17:00 +0100

    set production mode for login

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from gui.auth_window import AuthWindow
 from gui.main_window import App
 from logic.logger import logger, check_log_size
+from settings import settings as set
+
+
+def start_app(window: AuthWindow | App) -> None:
+    root = tk.Tk()
+    app = window(root)
+    root.mainloop()
+    return app
 
 
 if __name__ == "__main__":
@@ -9,13 +17,12 @@ if __name__ == "__main__":
 
     logger.info("============================================================")
 
-    root = tk.Tk()
-    login_window = AuthWindow(root)
-    root.mainloop()
+    if set.PRODUCTION_MODE_ON:
+        login_window = start_app(AuthWindow)
 
-    if login_window.auth_successful:  # ✅ Проверяем успешность авторизации
-        root = tk.Tk()
-        app = App(root)
-        root.mainloop()
+        if login_window.auth_successful:  # ✅ Проверяем успешность авторизации
+            start_app(App)
+        else:
+            logger.info("Application closed due to unsuccessful login")
     else:
-        logger.info("Application closed due to unsuccessful login")
+        start_app(App)
--- settings/settings.py
+++ settings/settings.py
@@ -295,3 +295,4 @@ LOGIN_ENTRIES = {
 }
 CREATE_USER_TITLE = "Create user"
 CREATE_USER_ENTRIES = ["Username", "Password", "Repeat password"]
+PRODUCTION_MODE_ON = False
