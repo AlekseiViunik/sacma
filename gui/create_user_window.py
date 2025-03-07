5affc5a821ae03d03420761597aa81f2dca77dd2
Aleksei VIunik <vjunik88@gmail.com>
2025-03-07 09:52:43 +0100

    add doctring and annotations to the CreateUserWindow

--- gui/create_user_window.py
+++ gui/create_user_window.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import messagebox
 
 from gui.type_classes.abstract_base_type import AbstractBaseType
@@ -5,11 +6,20 @@ from logic.authenticator import Authenticator
 
 
 class CreateUserWindow(AbstractBaseType):
-    def __init__(self, root, type=None, entry_widgets=None):
+    def __init__(
+        self,
+        root: tk.Tk,
+        type: str = None,
+        entry_widgets: list = None
+    ) -> None:
         super().__init__(root, type, entry_widgets)
         self.auth = Authenticator()
 
-    def calculate(self):
+    def calculate(self) -> None:
+        """
+        Регистрация новых юзеров. Выводит информационное окно в случае успешной
+        регистрации или окно об ошибке с ее описанием в случае провала.
+        """
         values = {key.lower(): var.get() for key, var in self.entries.items()}
         if values['password'] == values['repeat password']:
             if self.auth.register_user(values['username'], values['password']):
