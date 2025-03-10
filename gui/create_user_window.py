a9f55f3965b3ea9676ddef4622312a56fe8c0694
Aleksei VIunik <vjunik88@gmail.com>
2025-03-10 10:19:20 +0100

    add logging to the cfreate user window

--- gui/create_user_window.py
+++ gui/create_user_window.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 
 from gui.type_classes.abstract_base_type import AbstractBaseType
 from logic.authenticator import Authenticator
+from logic.logger import logger as log
 
 
 class CreateUserWindow(AbstractBaseType):
@@ -38,19 +39,23 @@ class CreateUserWindow(AbstractBaseType):
         Регистрация новых юзеров. Выводит информационное окно в случае успешной
         регистрации или окно об ошибке с ее описанием в случае провала.
         """
+        log.info("Trying to register new user")
         values = {key.lower(): var.get() for key, var in self.entries.items()}
         if values['password'] == values['repeat password']:
             if self.auth.register_user(values['username'], values['password']):
+                log.info("User is registered")
                 messagebox.showinfo(
                     "Done!",
                     f"User '{values['username']}' is created!"
                 )
             else:
+                log.error(f"User '{values['username']}' exists!")
                 messagebox.showerror(
                     "Error",
                     f"User '{values['username']}' exists!"
                 )
         else:
+            log.error("Passwords are different!")
             messagebox.showerror(
                 "Error",
                 "You entered different passwords!"
