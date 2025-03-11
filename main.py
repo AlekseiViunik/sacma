b5ecc322ee7a8a266b06176b445e52862465f6a3
Aleksei VIunik <vjunik88@gmail.com>
2025-03-11 15:29:25 +0100

    adapt main.py to use pyqt for custom implementation

--- main.py
+++ main.py
@@ -17,21 +17,28 @@ if __name__ == "__main__":
     logger.info("============================================================")
 
     if set.CUSTOM_IMPLEMENTATION:
+        from PyQt6.QtWidgets import QApplication
         from interface.custom_app import CustomApp
-        main_window = CustomApp
+        import sys
+
+        logger.info("Запуск нового интерфейса")
+        app = QApplication(sys.argv)
+        main_window = CustomApp()
+        main_window.show()
+        sys.exit(app.exec())
     else:
         from gui.main_window import App
         main_window = App
 
-    if set.PRODUCTION_MODE_ON:
-        logger.info("Trying to acces the app")
-        login_window = start_app(AuthWindow)
+        if set.PRODUCTION_MODE_ON:
+            logger.info("Trying to acces the app")
+            login_window = start_app(AuthWindow)
 
-        if login_window.auth_successful:
-            # ✅ Проверяем успешность авторизации
-            logger.info("Login is successful")
-            start_app(main_window)
+            if login_window.auth_successful:
+                # ✅ Проверяем успешность авторизации
+                logger.info("Login is successful")
+                start_app(main_window)
+            else:
+                logger.info("Application closed due to unsuccessful login")
         else:
-            logger.info("Application closed due to unsuccessful login")
-    else:
-        start_app(main_window)
+            start_app(main_window)
