1a54e0fcd9d23b5d3202d8cb127e00e849e3765b
Aleksei VIunik <vjunik88@gmail.com>
2025-03-11 15:29:57 +0100

    first version of custom app

--- interface/custom_app.py
+++ interface/custom_app.py
@@ -1,2 +1,29 @@
-class CustomApp:
-    pass
+import sys
+
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from interface.windows.settings_window import SettingsWindow
+
+
+class CustomApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("Главное окно")
+        self.setGeometry(100, 100, 400, 400)
+
+        self.settings_button = QPushButton("Настройки", self)
+        self.settings_button.setGeometry(150, 350, 100, 30)
+        self.settings_button.clicked.connect(self.open_settings)
+
+    def open_settings(self):
+        self.settings_window = SettingsWindow()
+        self.settings_window.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = CustomApp()
+    main_window.show()
+    sys.exit(app.exec())
