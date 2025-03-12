6077cdbd16c0caaa27ecca019946c04eaaa5630c
Aleksei VIunik <vjunik88@gmail.com>
2025-03-12 16:50:39 +0100

    set main window to use config instead of directly put the widgets

--- interface/custom_app.py
+++ interface/custom_app.py
@@ -1,9 +1,9 @@
-import sys
-
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt6.QtWidgets import QWidget
 
 from interface.windows.settings_window import SettingsWindow
 from handlers.json_handler import JsonHandler
+from interface.creator import Creator
+from interface.helper import Helper
 
 CONFIG_FILE = "windows_configs/main_window.json"
 
@@ -11,27 +11,32 @@ CONFIG_FILE = "windows_configs/main_window.json"
 class CustomApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.width = 450
-        self.height = 150
+        self.width = 0
+        self.height = 0
         self.config_json_handler = JsonHandler(CONFIG_FILE)
         self.creator = None
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Главное окно")
-        self.setGeometry(100, 100, 400, 400)
+        # Загружаем конфиг
+        config = self.config_json_handler.get_all_data()
 
-        self.settings_button = QPushButton("Настройки", self)
-        self.settings_button.setGeometry(150, 350, 100, 30)
-        self.settings_button.clicked.connect(self.open_settings)
+        # Настраиваем окно
+        self.setWindowTitle(config['window_title'])
+        self.window_width = int(config['window_width'])
+        self.window_height = int(config['window_height'])
+        Helper.move_window_to_center(self)
+
+        # Создаем слои и виджеты через креатор
+        self.creator = Creator(config, self)
+        self.creator.create_widget_layout(self, config["layout"])
 
     def open_settings(self):
         self.settings_window = SettingsWindow()
         self.settings_window.show()
 
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_window = CustomApp()
-    main_window.show()
-    sys.exit(app.exec())
+    def connect_callback(self, button, callback_name, params):
+        if callback_name == "open_settings":
+            button.clicked.connect(self.open_settings)
+        else:
+            pass
