5f4d481eba98c99bb366c25bf8bacc2a7c21f818
Aleksei VIunik <vjunik88@gmail.com>
2025-03-13 08:40:08 +0100

    add annotations

--- interface/custom_app.py
+++ interface/custom_app.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QPushButton
 
 from interface.windows.settings_window import SettingsWindow
 from handlers.json_handler import JsonHandler
@@ -9,7 +9,7 @@ CONFIG_FILE = "windows_configs/main_window.json"
 
 
 class CustomApp(QWidget):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.width = 0
         self.height = 0
@@ -17,7 +17,7 @@ class CustomApp(QWidget):
         self.creator = None
         self.init_ui()
 
-    def init_ui(self):
+    def init_ui(self) -> None:
         # Загружаем конфиг
         config = self.config_json_handler.get_all_data()
 
@@ -31,12 +31,25 @@ class CustomApp(QWidget):
         self.creator = Creator(config, self)
         self.creator.create_widget_layout(self, config["layout"])
 
-    def open_settings(self):
+    def open_settings(self) -> None:
         self.settings_window = SettingsWindow()
         self.settings_window.show()
 
-    def connect_callback(self, button, callback_name, params):
+    def open_input_window(self) -> None:
+        sender = self.sender()  # Получаем объект кнопки
+        if sender:
+            window_name = sender.text()  # Берем текст кнопки как имя окна
+            print(f"Открытие окна: {window_name}")
+
+    def connect_callback(
+        self,
+        button: QPushButton,
+        callback_name: str,
+        params: dict
+    ) -> None:
         if callback_name == "open_settings":
             button.clicked.connect(self.open_settings)
+        elif callback_name == "open_input_window":
+            button.clicked.connect(self.open_input_window)
         else:
             pass
