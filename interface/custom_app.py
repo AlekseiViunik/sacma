0516c38e6461913b089093da83fc776ef9eb06e6
Aleksei VIunik <vjunik88@gmail.com>
2025-03-13 09:09:25 +0100

    add logic to the open_input_window

--- interface/custom_app.py
+++ interface/custom_app.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QWidget, QPushButton
 
+from interface.windows.input_window import InputWindow
 from interface.windows.settings_window import SettingsWindow
 from handlers.json_handler import JsonHandler
 from interface.creator import Creator
@@ -35,11 +36,16 @@ class CustomApp(QWidget):
         self.settings_window = SettingsWindow()
         self.settings_window.show()
 
-    def open_input_window(self) -> None:
+    def open_input_window(self, params) -> None:
         sender = self.sender()  # Получаем объект кнопки
         if sender:
             window_name = sender.text()  # Берем текст кнопки как имя окна
-            print(f"Открытие окна: {window_name}")
+            input_window = InputWindow(
+                window_name,
+                params['json_file_path'],
+                self
+            )
+            input_window.init_ui()
 
     def connect_callback(
         self,
@@ -50,6 +56,6 @@ class CustomApp(QWidget):
         if callback_name == "open_settings":
             button.clicked.connect(self.open_settings)
         elif callback_name == "open_input_window":
-            button.clicked.connect(self.open_input_window)
+            button.clicked.connect(lambda: self.open_input_window(params))
         else:
             pass
