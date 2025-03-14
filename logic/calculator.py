697ae88dabe5c4878c490ce6884ff3d44f238bf1
Aleksei Viunik <vjunik88@gmail.com>
2025-03-14 16:58:55 +0100

    add new properties to calculator and logic to its calc_data method

--- logic/calculator.py
+++ logic/calculator.py
@@ -1,11 +1,21 @@
+from handlers.json_handler import JsonHandler
 from helpers.helper import Helper
 
 
 class Calculator:
-    def __init__(self, data, type) -> None:
+    def __init__(self, data, type, choices) -> None:
         self.data: dict = data
         self.type: str = type
-        self.calc_file_path = Helper.get_calculation_file(self.type)
+        self.choices: dict = choices
+        self.calc_file_path: str = Helper.get_calculation_file(self.type)
+        self.calc_config: dict = {}
+        self.config_file_handler = JsonHandler(self.calc_file_path)
 
     def calc_data(self):
+        self.calc_config = self.config_file_handler.get_all_data()
+        keys = list(self.choices.values())
+        self.calc_config = self.calc_config = Helper.get_nested_data(
+            keys,
+            self.calc_config
+        )
         pass
