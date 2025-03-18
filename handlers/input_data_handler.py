969a3d666c17b293d28b41b5f3bd379c2c2b4346
Aleksei Viunik <vjunik88@gmail.com>
2025-03-18 16:14:31 +0100

    add check_mandatory and showboxes methods

--- handlers/input_data_handler.py
+++ handlers/input_data_handler.py
@@ -1,3 +1,7 @@
+from PyQt6.QtWidgets import (
+    QMessageBox
+)
+
 from helpers.remover import Remover
 
 
@@ -17,3 +21,22 @@ class InputDataHandler:
             all_inputs[name] = field.text()
         return all_inputs
 
+    def check_mandatory(self, inputs, mandatories):
+        filled_inputs = {k for k, v in inputs.items() if v}
+        return list(set(mandatories) - filled_inputs)
+
+    def show_error_messagebox(self, title, msg, window):
+        box = QMessageBox(window)
+        box.setWindowTitle(title)
+        box.setText(msg)
+        box.setIcon(QMessageBox.Icon.Critical)
+        box.setStandardButtons(QMessageBox.StandardButton.Ok)
+        box.show()
+
+    def show_success_messagebox(self, title, msg, window):
+        box = QMessageBox(window)
+        box.setWindowTitle(title)
+        box.setText(msg)
+        box.setIcon(QMessageBox.Icon.Information)
+        box.setStandardButtons(QMessageBox.StandardButton.Ok)
+        box.exec()
