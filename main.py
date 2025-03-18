a4f8e6874b7b4b2bc94315ac3b31174e4475dc82
Aleksei Viunik <vjunik88@gmail.com>
2025-03-18 12:18:23 +0100

    activate login before the main window if production mode is on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gui.auth_window import AuthWindow
+from interface.windows.login_window import LoginWindow
 from logic.logger import logger, check_log_size
 from settings import settings as set
 
@@ -21,11 +22,26 @@ if __name__ == "__main__":
         from interface.custom_app import CustomApp
         import sys
 
-        logger.info("Запуск нового интерфейса")
-        app = QApplication(sys.argv)
-        main_window = CustomApp()
-        main_window.show()
-        sys.exit(app.exec())
+        if set.PRODUCTION_MODE_ON:
+            logger.info("Trying to acces the app")
+            app = QApplication(sys.argv)
+            login_window = LoginWindow()
+            login_window.show()
+            app.exec()
+            if login_window.auth_successful:
+                logger.info("Login is successful")
+                main_window = CustomApp()
+                main_window.show()
+                sys.exit(app.exec())
+            else:
+                logger.info("Application closed due to unsuccessful login")
+
+        else:
+            logger.info("Запуск нового интерфейса")
+            app = QApplication(sys.argv)
+            main_window = CustomApp()
+            main_window.show()
+            sys.exit(app.exec())
     else:
         from gui.main_window import App
         main_window = App
