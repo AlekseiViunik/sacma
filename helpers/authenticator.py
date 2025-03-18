2f9a9cdcf085063eb5ee8a5a386caf9aa14106a4
Aleksei Viunik <vjunik88@gmail.com>
2025-03-18 12:19:17 +0100

    add a duplicate of the authenticator

---
+++ helpers/authenticator.py
@@ -0,0 +1,72 @@
+import hashlib
+import os
+
+from handlers.json_handler import JsonHandler
+from logic.logger import logger as log
+from settings import settings as set
+
+
+class Authenticator:
+    """
+    Класс для управления авторизацией пользователей.
+    """
+    def __init__(self):
+        self.file_handler = JsonHandler(set.AUTH_FILE)
+
+    def load_users(self):
+        """
+        Загружает пользователей из auth.json.
+        """
+        if not os.path.exists(set.AUTH_FILE):
+            return {"users": {}, "lastUser": ""}
+
+        return self.file_handler.get_all_data()
+
+    def load_last_user(self) -> str:
+        last_user = ""
+        if last_user := self.file_handler.get_value_by_key("lastUser"):
+            log.info(f"Last user found {last_user}")
+        return self.file_handler.get_value_by_key("lastUser")
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        """
+        Хеширует пароль с помощью SHA-256.
+        """
+        log.info("Hashing the pass")
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    @staticmethod
+    def verify_user(username: str, password: str) -> bool:
+        """
+        Проверяет логин и пароль пользователя.
+        """
+        users_data = Authenticator().load_users()
+        hashed_password = Authenticator().hash_password(password)
+        log.info(f"Check if the user {username} with pass '{password}' exists")
+        return users_data['users'].get(username) == hashed_password
+
+    def save_last_user(self, username: str) -> None:
+        """
+        Сохраняет последнего вошедшего пользователя.
+        """
+        log.info("Save last user")
+        self.file_handler.write_into_file(key="lastUser", value=username)
+
+    def register_user(self, username: str, password: str) -> bool:
+        """
+        Регистрирует нового пользователя (если его нет).
+        """
+
+        users_data = Authenticator().load_users()
+
+        if username in users_data['users']:
+            return False  # Пользователь уже существует
+
+        users_data['users'][username] = self.file_handler.write_into_file(
+            'users',
+            username,
+            Authenticator().hash_password(password)
+        )
+
+        return True
