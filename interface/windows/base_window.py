d633d3178b657fe5c0546fb9b75199eb92d08336
Aleksei Viunik <vjunik88@gmail.com>
2025-03-19 15:21:48 +0100

    create base window

---
+++ interface/windows/base_window.py
@@ -0,0 +1,43 @@
+from PyQt6.QtWidgets import QWidget
+from handlers.json_handler import JsonHandler
+from interface.creator import Creator
+from helpers.helper import Helper
+from logic.logger import logger as log
+
+
+class BaseWindow(QWidget):
+    """Базовое окно для всех окон приложения."""
+
+    CONFIG_FILE = None  # Путь к конфигу должен задаваться в наследниках
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.window_width = 0
+        self.window_height = 0
+        # Инициализируем обработчик JSON
+        self.config_json_handler = JsonHandler(self.CONFIG_FILE)
+        self.creator = None
+
+    def init_ui(self):
+        """Создает интерфейс окна на основе JSON-конфига."""
+
+        log.info(f"Creating window with config: {self.CONFIG_FILE}")
+
+        # Загружаем конфиг
+        config = self.config_json_handler.get_all_data()
+
+        if config:
+            log.info(f"Config loaded successfully: {config}")
+        else:
+            log.error("Couldn't get the data from the file!")
+            return
+
+        self.setWindowTitle(config['window_title'])
+        self.window_width = int(config['window_width'])
+        self.window_height = int(config['window_height'])
+
+        Helper.move_window_to_top_left_corner(self)
+
+        log.info("Using creator to generate UI layout")
+        self.creator = Creator(config, self)
+        self.creator.create_widget_layout(self, config["layout"])
