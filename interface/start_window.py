00625f00e4d61e313119237b9bb0167e011e6f82
Aleksei Viunik <vjunik88@gmail.com>
2025-03-21 15:18:13 +0100

    add Docstrings, comments and annotations to the start window and move its connect callback to the base window

--- interface/start_window.py
+++ interface/start_window.py
@@ -1,32 +1,57 @@
-from PyQt6.QtWidgets import QPushButton
-
+from typing import Dict
 from interface.windows.input_window import InputWindow
 from interface.windows.register_window import RegisterWindow
 from interface.windows.settings_window import SettingsWindow
-from handlers.json_handler import JsonHandler
 from settings import settings as set
 from logic.logger import logger as log
 from interface.windows.base_window import BaseWindow
 
 
 class StartWindow(BaseWindow):
+    """
+    Стартовое окно с кнопками открытия второстепенных окон.
+    Своих атрибутов нет.
+
+    Methods
+    -------
+    - open_settings(target_input)
+        Открывает окно пользовательских настроек.
+
+    - open_input_window()
+        Открывает выбранное окно ввода данных.
+
+    - open_register()
+        Открывает окно регистрации нового юзера.
+    """
 
     CONFIG_FILE = set.MAIN_WINDOW_CONFIG_FILE
 
     def __init__(self) -> None:
         super().__init__()
-        self.width = 0
-        self.height = 0
-        self.config_json_handler = JsonHandler(set.MAIN_WINDOW_CONFIG_FILE)
-        self.creator = None
+        # Своих атрибутов у класса нет.
+
         self.init_ui()
 
     def open_settings(self) -> None:
+        """
+        Открывает окно пользовательских настроек.
+        """
+
         log.info("Settings button has been pressed!")
         self.settings_window = SettingsWindow()
         self.settings_window.show()
 
-    def open_input_window(self, params) -> None:
+    def open_input_window(self, params: Dict[str, str]) -> None:
+        """
+        Открывает выбранное окно ввода данных.
+
+        Parameters
+        ----------
+        params: Dict[str, str]
+            Параметры для кнопки окна открытия. По сути содержат пока что
+            только одно значение - путь к файлу с конфигом этого окна.
+        """
+
         sender = self.sender()  # Получаем объект кнопки
         log.info(f"{sender.text()} button has been pressed!")
         if sender:
@@ -37,23 +62,10 @@ class StartWindow(BaseWindow):
             )
         input_window.show()
 
-    def open_register(self):
+    def open_register(self) -> None:
+        """
+        Открывает окно регистрации нового юзера.
+        """
         log.info("Create user button has been pressed!")
         self.register_window = RegisterWindow()
         self.register_window.show()
-
-    def connect_callback(
-        self,
-        button: QPushButton,
-        callback_name: str,
-        params: dict = {},
-        parent=None
-    ) -> None:
-        if callback_name == "open_settings":
-            button.clicked.connect(self.open_settings)
-        elif callback_name == "open_input_window":
-            button.clicked.connect(lambda: self.open_input_window(params))
-        elif callback_name == "open_register":
-            button.clicked.connect(self.open_register)
-        else:
-            pass
