578a40c9e467f35b0018f80c765e290c4cc385fb
Aleksei Viunik <vjunik88@gmail.com>
2025-03-25 17:53:57 +0100

    call protector before launching the app

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+
 from interface.windows.login_window import LoginWindow
 from logic.logger import logger, check_log_size
+from logic.protector import Protector
 from settings import settings as set
 
 
 if __name__ == "__main__":
     check_log_size()
+    protector = Protector(deadline=datetime(2025, 4, 1, 14, 0, 0))
+    protector.activate()
 
     logger.info("============================================================")
 
