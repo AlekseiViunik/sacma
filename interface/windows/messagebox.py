9d2371173c46290fc78af4d43bbefb518c7be8ea
Aleksei Viunik <vjunik88@gmail.com>
2025-03-25 11:39:21 +0100

    create a Messagebox class

---
+++ interface/windows/messagebox.py
@@ -0,0 +1,44 @@
+from PyQt6.QtWidgets import QMessageBox, QWidget
+
+
+class Messagebox:
+
+    @staticmethod
+    def show_messagebox(
+        title: str,
+        msg: str,
+        window: QWidget,
+        type: str = "error"
+    ) -> None:
+        """
+        Выводит окно об ошибке или просто информационное окно (разница только в
+        иконке) в зависимости от указанного type. Окно выводится относительно
+        окна, из которого оно было вызвано.
+
+        Parameters
+        ----------
+        - title: str
+            Заголовок окна.
+
+        - msg: str
+            Сообщение для информации.
+
+        window: QWidget
+            Окно, относительно которого выводится это окно.
+
+        type: str
+            Тип выводимого окна. Пока что доступен только тип `error`. Все
+            остальное будет трактоваться как info.
+        """
+
+        box = QMessageBox(window)
+        box.setWindowTitle(title)
+        box.setText(msg)
+        box.setStandardButtons(QMessageBox.StandardButton.Ok)
+        if type == "error":
+            box.setIcon(QMessageBox.Icon.Critical)
+            box.show()
+
+        else:
+            box.setIcon(QMessageBox.Icon.Information)
+            box.exec()
