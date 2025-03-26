5ce65cc7b0aa460cdad12208dc0165bc8e27d852
Aleksei Viunik <vjunik88@gmail.com>
2025-03-26 17:24:48 +0100

    add noinspection to the conftest_imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,14 @@
+# noinspection PyUnresolvedReferences
+import tests.conftest_imports  # noqa: F401
+
 import pytest
-import sys
-
-from pathlib import Path
 from PyQt6.QtWidgets import QApplication
-
 from interface.start_window import StartWindow
-
-
-# Добавляем корень проекта в sys.path. Нужно для правильного использования
-# импортов основного приложения.
-root = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(root))
 
 
 @pytest.fixture(scope="session")
 def app():
-    return QApplication(sys.argv)
+    return QApplication([])
 
 
 @pytest.fixture
