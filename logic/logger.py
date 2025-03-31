bfe187f86ceb325308b0b69ba5630766540bbd47
Aleksei Viunik <vjunik88@gmail.com>
2025-03-31 15:20:39 +0200

    rename settings alias in logger

--- logic/logger.py
+++ logic/logger.py
@@ -2,26 +2,26 @@ import logging
 import os
 import sys
 
-from settings import settings as set
+from settings import settings as sett
 
 # Путь к файлу логов.
 # Если приложение запущено как ехе, то путь будет другой.
 if getattr(sys, 'frozen', False):
     BASE_DIR = os.path.dirname(sys.executable)
-    LOG_DIR = os.path.join(BASE_DIR, set.LOGS_FOLDER_NAME)
+    LOG_DIR = os.path.join(BASE_DIR, sett.LOGS_FOLDER_NAME)
 else:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     LOG_DIR = os.path.join(
         BASE_DIR,
-        set.ONE_LEVEL_UP_FOLDER,
-        set.LOGS_FOLDER_NAME
+        sett.ONE_LEVEL_UP_FOLDER,
+        sett.LOGS_FOLDER_NAME
     )
 
 # Создаём папку logs, если её нет.
 os.makedirs(LOG_DIR, exist_ok=True)
 
 # Путь к файлу логов.
-LOG_FILE = os.path.join(LOG_DIR, set.LOG_FILE_NAME)
+LOG_FILE = os.path.join(LOG_DIR, sett.LOG_FILE_NAME)
 
 
 def check_log_size() -> None:
@@ -31,12 +31,12 @@ def check_log_size() -> None:
     начинает писаться заново.
     """
     if os.path.exists(LOG_FILE):
-        with open(LOG_FILE, set.FILE_READ, encoding=set.STR_CODING) as f:
+        with open(LOG_FILE, sett.FILE_READ, encoding=sett.STR_CODING) as f:
             lines = f.readlines()
 
-        if len(lines) >= set.MAX_LOG_LINES:
-            with open(LOG_FILE, set.FILE_WRITE, encoding=set.STR_CODING) as f:
-                f.write(set.EMPTY_STRING)  # Очищаем файл
+        if len(lines) >= sett.MAX_LOG_LINES:
+            with open(LOG_FILE, sett.FILE_WRITE, encoding=sett.STR_CODING) as f:
+                f.write(sett.EMPTY_STRING)  # Очищаем файл
 
 
 # Настройка логирования
@@ -44,9 +44,9 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",  # Формат сообщения
     handlers=[
-        logging.FileHandler(LOG_FILE, encoding=set.STR_CODING),  # Логи в файл
+        logging.FileHandler(LOG_FILE, encoding=sett.STR_CODING),  # Логи в файл
     ]
 )
 
 # Создаём логгер
-logger = logging.getLogger(set.APP_LOGGER)
+logger = logging.getLogger(sett.APP_LOGGER)
