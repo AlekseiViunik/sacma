87d098d34b39611001821c5d671d11c662c15b83
Aleksei Viunik <vjunik88@gmail.com>
2025-04-01 09:59:03 +0200

    remove string handler

--- handlers/string_handler.py
+++
@@ -1,9 +0,0 @@
-class StringHandler:
-    """
-    Класс для работы с динамическими строками.
-    """
-    def create_dynamic_message(static_message, *args):
-        """
-        Создает динамическое сообщение, подставляя значения из args в static_message.
-        """
-        return static_message.format(*args)
