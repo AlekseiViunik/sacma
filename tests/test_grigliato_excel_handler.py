e97dec14038438a27f5826e4f11957fce6b3cb57
Aleksei Viunik <vjunik88@gmail.com>
2025-04-01 16:41:25 +0200

    add the rest tests of the grigliato calculation

--- tests/test_grigliato_excel_handler.py
+++ tests/test_grigliato_excel_handler.py
@@ -184,3 +184,187 @@ def test_grigliato_wrong_data_passed():
         f"Ожидалось: {expected_post_message}\n"
         f"Нашлось: {post_message}"
     )
+
+
+def test_bugnato_excel_calculation_1():
+    data = {
+        'type': 'Bugnato',
+        'thickness': '2.0',
+        'base': '250x50',
+        'length': '3000'
+    }
+
+    choices = {'type': 'Bugnato'}
+    el_type = 'Grigliato'
+
+    calculator = Calculator(data, el_type, choices)
+    data_to_check, post_message = calculator.calc_data()
+
+    expected_result = {'price': Decimal('40.77'), 'weight': Decimal('17.46')}
+    expected_post_message = None
+
+    assert data_to_check, (
+        "Метод initiate_process() вернул пустой результат или None"
+    )
+
+    assert len(data_to_check) == len(expected_result), (
+        f"Количество данных не совпадает.\n"
+        f"Ожидалось: {len(expected_result)}\n"
+        f"Нашлось: {len(data_to_check)}"
+    )
+
+    assert data_to_check['price'] == expected_result['price'], (
+        f"Цена не совпадает.\n"
+        f"Ожидалось: {expected_result['price']}\n"
+        f"Нашлось: {data_to_check['price']}"
+    )
+
+    assert data_to_check['weight'] == expected_result['weight'], (
+        f"Вес не совпадает.\n"
+        f"Ожидалось: {expected_result['weight']}\n"
+        f"Нашлось: {data_to_check['weight']}"
+    )
+
+    assert post_message == expected_post_message, (
+        f"Сообщение не совпадает.\n"
+        f"Ожидалось: {expected_post_message}\n"
+        f"Нашлось: {post_message}"
+    )
+
+
+def test_bugnato_excel_calculation_2():
+    data = {
+        'type': 'Bugnato',
+        'thickness': '3.0',
+        'base': '270x50',
+        'length': '680'
+    }
+
+    choices = {'type': 'Bugnato'}
+    el_type = 'Grigliato'
+
+    calculator = Calculator(data, el_type, choices)
+    data_to_check, post_message = calculator.calc_data()
+
+    expected_result = {'price': Decimal('15.91'), 'weight': Decimal('6.26')}
+    expected_post_message = 'N.B.! Solo grandi quantitativi. Contatare sede!'
+
+    assert data_to_check, (
+        "Метод initiate_process() вернул пустой результат или None"
+    )
+
+    assert len(data_to_check) == len(expected_result), (
+        f"Количество данных не совпадает.\n"
+        f"Ожидалось: {len(expected_result)}\n"
+        f"Нашлось: {len(data_to_check)}"
+    )
+
+    assert data_to_check['price'] == expected_result['price'], (
+        f"Цена не совпадает.\n"
+        f"Ожидалось: {expected_result['price']}\n"
+        f"Нашлось: {data_to_check['price']}"
+    )
+
+    assert data_to_check['weight'] == expected_result['weight'], (
+        f"Вес не совпадает.\n"
+        f"Ожидалось: {expected_result['weight']}\n"
+        f"Нашлось: {data_to_check['weight']}"
+    )
+
+    assert post_message == expected_post_message, (
+        f"Сообщение не совпадает.\n"
+        f"Ожидалось: {expected_post_message}\n"
+        f"Нашлось: {post_message}"
+    )
+
+
+def test_bugnato_excel_calculation_3():
+    data = {
+        'type': 'Bugnato',
+        'thickness': '3.0',
+        'base': '270x40',
+        'length': '6000'
+    }
+
+    choices = {'type': 'Bugnato'}
+    el_type = 'Grigliato'
+
+    calculator = Calculator(data, el_type, choices)
+    data_to_check, post_message = calculator.calc_data()
+
+    expected_result = {'price': None, 'weight': None}
+    expected_post_message = 'ATTENZIONE! Per la base 270x40 contatare sede!'
+
+    assert data_to_check, (
+        "Метод initiate_process() вернул пустой результат или None"
+    )
+
+    assert len(data_to_check) == len(expected_result), (
+        f"Количество данных не совпадает.\n"
+        f"Ожидалось: {len(expected_result)}\n"
+        f"Нашлось: {len(data_to_check)}"
+    )
+
+    assert data_to_check['price'] == expected_result['price'], (
+        f"Цена не совпадает.\n"
+        f"Ожидалось: {expected_result['price']}\n"
+        f"Нашлось: {data_to_check['price']}"
+    )
+
+    assert data_to_check['weight'] == expected_result['weight'], (
+        f"Вес не совпадает.\n"
+        f"Ожидалось: {expected_result['weight']}\n"
+        f"Нашлось: {data_to_check['weight']}"
+    )
+
+    assert post_message == expected_post_message, (
+        f"Сообщение не совпадает.\n"
+        f"Ожидалось: {expected_post_message}\n"
+        f"Нашлось: {post_message}"
+    )
+
+
+def test_bugnato_wrong_data_passed():
+    data = {
+        'type': 'Bugnato',
+        'thickness': '3.0',
+        'base': '270x40',
+        'length': '6100'
+    }
+
+    choices = {'type': 'Bugnato'}
+    el_type = 'Grigliato'
+
+    calculator = Calculator(data, el_type, choices)
+    data_to_check, post_message = calculator.calc_data()
+
+    expected_result = {'price': None, 'weight': None}
+    expected_post_message = 'Lunghezza should be less than 6000. You have 6100'
+
+    assert data_to_check, (
+        "Метод initiate_process() вернул пустой результат или None"
+    )
+
+    assert len(data_to_check) == len(expected_result), (
+        f"Количество данных не совпадает.\n"
+        f"Ожидалось: {len(expected_result)}\n"
+        f"Нашлось: {len(data_to_check)}"
+    )
+
+    assert data_to_check['price'] == expected_result['price'], (
+        f"Цена не совпадает.\n"
+        f"Ожидалось: {expected_result['price']}\n"
+        f"Нашлось: {data_to_check['price']}"
+    )
+
+    assert data_to_check['weight'] == expected_result['weight'], (
+        f"Вес не совпадает.\n"
+        f"Ожидалось: {expected_result['weight']}\n"
+        f"Нашлось: {data_to_check['weight']}"
+    )
+
+    assert post_message == expected_post_message, (
+        f"Сообщение не совпадает.\n"
+        f"Ожидалось: {expected_post_message}\n"
+        f"Нашлось: {post_message}"
+    )
