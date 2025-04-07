0d53ce19c2917b76a7ffe35b213fb094c70dfa5b
Aleksei Viunik <vjunik88@gmail.com>
2025-04-07 10:03:25 +0200

    add check_excel_file method and call it before launching the app

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from datetime import datetime
 
+from handlers.json_handler import JsonHandler
 from interface.windows.login_window import LoginWindow
+from interface.windows.settings_window import SettingsWindow
 from logic.logger import logger, check_log_size
 from logic.protector import Protector
 from settings import settings as sett
 
 
 if __name__ == "__main__":
+
+    def check_excel_file() -> None:
+        """
+        Проверяет, существует ли файл Excel, указанный в настройках.
+        Если файла нет, открывает окно настроек.
+        """
+        settings_json_handler = JsonHandler(sett.SETTINGS_FILE)
+        if not settings_json_handler.get_value_by_key(sett.EXCEL_PATH):
+            logger.info(sett.SETTINGS_BUTTON_PRESSED)
+            settings_window = SettingsWindow()
+            settings_window.exec()
+
     check_log_size()
     if sett.PRODUCTION_MODE_ON:
         protector = Protector(
@@ -32,6 +46,7 @@ if __name__ == "__main__":
         app = QApplication(sys.argv)
         login_window = LoginWindow()
         if login_window.exec():
+            check_excel_file()
             logger.info(sett.SUCCESSFUL_LOGIN)
 
             main_window = StartWindow(login_window.username)
@@ -43,6 +58,7 @@ if __name__ == "__main__":
     else:
         logger.info(sett.NEW_APP_START)
         app = QApplication(sys.argv)
+        check_excel_file()
         main_window = StartWindow()
         main_window.show()
         sys.exit(app.exec())
