6fde597c4e394e0d3b7f228e8ca41ae847382e1c
Aleksei Viunik <vjunik88@gmail.com>
2025-04-07 14:34:08 +0200

    add excel_handler fixture for the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import logging
 import pytest
 
 from PyQt6.QtWidgets import QApplication
+
+from handlers.excel_handler import ExcelHandler
 from interface.start_window import StartWindow
 
 
@@ -24,3 +26,11 @@ def start_window(qtbot):
 @pytest.fixture(autouse=True, scope="session")
 def disable_logging():
     logging.disable(logging.CRITICAL)
+
+
+@pytest.fixture(scope="session")
+def excel_handler():
+    handler = ExcelHandler()
+    handler.open_excel()  # Открываем один раз
+    yield handler
+    handler.close_excel()
