38094f987ca7d40523f8be29aafe8fcdb2436501
Aleksei Viunik <vjunik88@gmail.com>
2025-04-11 10:15:31 +0200

    create user settings window class

---
+++ interface/windows/users_settings_window.py
@@ -0,0 +1,52 @@
+from interface.windows.messagebox import Messagebox
+from .base_window import BaseWindow
+from handlers.json_handler import JsonHandler
+from logic.logger import logger as log
+from settings import settings as sett
+
+
+class UsersSettingsWindow(BaseWindow):
+
+    def __init__(
+        self,
+        window_name: str,
+        file_path: str,
+    ) -> None:
+        super().__init__(file_path)
+        self.window_name: str = window_name
+
+        self.init_ui()
+
+    def save_users_settings(self) -> None:
+        user_data_json_handler = JsonHandler(
+            sett.USER_MAIN_DATA_FILE, True
+        )
+
+        try:
+            username = self.creator.chosen_fields[sett.USERNAME].currentText()
+            usergroup = (
+                self.creator.chosen_fields[sett.USERGROUP].currentText()
+            )
+            userdata = user_data_json_handler.get_all_data()
+            userdata[username][sett.GROUP] = usergroup
+
+            user_data_json_handler.rewrite_file(userdata)
+
+        except Exception as e:
+            log.error(sett.SAVE_USER_SETTINGS_ERROR.format(username, e))
+            Messagebox.show_messagebox(
+                sett.CHANGE_GROUP_ERROR,
+                sett.SAVE_USER_SETTINGS_ERROR.format(username, e),
+                None,
+                exec=True
+            )
+            return
+
+        Messagebox.show_messagebox(
+            sett.SUCCESS,
+            sett.SAVE_USER_SETTINGS_SUCCESS.format(username),
+            None,
+            type=sett.TYPE_INFO,
+            exec=True
+        )
+        self.close()
