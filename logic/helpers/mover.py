49cc75bfaada62663c3fa1673ee002730553a45f
Aleksei Viunik <vjunik88@gmail.com>
2025-04-14 14:35:10 +0200

    create Mover

---
+++ logic/helpers/mover.py
@@ -0,0 +1,65 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+
+from settings import settings as sett
+
+
+class Mover:
+
+    @staticmethod
+    def move_window_to_center(window: QWidget) -> None:
+        """
+        Сдвигает в центр указанное окно.
+
+        Parameters
+        ----------
+        - window: QWidget
+            Окно, которое нужно сдвинуть.
+        """
+        screen_geometry = QApplication.primaryScreen().geometry()
+
+        x = (
+            screen_geometry.width() - window.window_width
+        ) // sett.MIDDLE_DETERMINANT_DIVIDER
+
+        y = (
+            screen_geometry.height() - window.window_height
+        ) // sett.MIDDLE_DETERMINANT_DIVIDER
+
+        window.setGeometry(x, y, window.window_width, window.window_height)
+
+    @staticmethod
+    def move_window_to_top_left_corner(window: QWidget) -> None:
+        """
+        Сдвигает в верхний левый угол указанное окно.
+
+        Parameters
+        ----------
+        - window: QWidget
+            Окно, которое нужно сдвинуть.
+        """
+        window.setGeometry(
+            sett.TOP_LEFT_X,
+            sett.TOP_Y,
+            window.window_width,
+            window.window_height
+        )
+
+    @staticmethod
+    def move_window_to_top_center(window: QWidget) -> None:
+        """
+        Сдвигает в верхний центр указанное окно.
+
+        Parameters
+        ----------
+        - window: QWidget
+            Окно, которое нужно сдвинуть.
+        """
+        screen_geometry = QApplication.primaryScreen().geometry()
+
+        x = (
+            screen_geometry.width() - window.window_width
+        ) // sett.MIDDLE_DETERMINANT_DIVIDER
+
+        y = sett.TOP_Y
+
+        window.setGeometry(x, y, window.window_width, window.window_height)
