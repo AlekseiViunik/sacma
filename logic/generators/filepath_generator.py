c8b7f81f8061b9367a98487800b45178561f6462
Aleksei Viunik <vjunik88@gmail.com>
2025-04-18 15:25:15 +0200

    add docstrings to the Filepath generator. Add generate_log_filepath and generate_temp_excel_filepath methods as well

--- logic/generators/filepath_generator.py
+++ logic/generators/filepath_generator.py
@@ -1,3 +1,6 @@
+import os
+import tempfile
+
 from settings import settings as sett
 
 
@@ -7,13 +10,72 @@ class FilepathGenerator:
 
     Methods
     -------
-    - generate_config_filepath(path, username)
+    - generate_settings_filepath(path, username)
         Генерирует путь к файлу конфигурации для указанного пользователя.
+
+    - generate_log_filepath(path, username)
+        Генерирует путь к файлу журнала логов для указанного пользователя.
+
+    - generate_temp_excel_filepath()
+        Генерирует путь к временной директории для Excel файла.
     """
 
     @staticmethod
     def generate_settings_filepath(path: str, username: str) -> str:
+        """
+        Генерирует путь к файлу настроек для указанного пользователя.
+
+        Parameters
+        ----------
+        - path: str
+            Путь к файлу настроек.
+
+        - username: str
+            Имя пользователя, для которого генерируется путь.
+
+        Returns
+        -------
+        - _: str
+            Путь к файлу настроек для указанного пользователя.
+        """
+
         return path.replace(
             sett.SETTINGS_JSON,
             sett.MODIFIED_SETTINGS_JSON.format(username)
         )
+
+    @staticmethod
+    def generate_log_filepath(path: str, username: str) -> str:
+        """
+        Генерирует путь к файлу журнала логов для указанного пользователя.
+
+        Parameters
+        ----------
+        - path: str
+            Путь к папке с логами.
+
+        - username: str
+            Имя пользователя, для которого генерируется путь.
+
+        Returns
+        -------
+        - _: str
+            Путь к файлу журнала логов для указанного пользователя.
+        """
+
+        return os.path.join(path, sett.MODIFIED_LOG_PATH.format(username))
+
+    @staticmethod
+    def generate_temp_excel_filepath() -> str:
+        """
+        Генерирует путь к временной директории для Excel файла. Для
+        генерации используется временная директория операционной
+        системы.
+
+        Returns
+        -------
+        - _: str
+            Путь к временной директории для Excel файла.
+        """
+
+        return os.path.join(tempfile.gettempdir(), sett.TEMP_EXCEL_NAME)
